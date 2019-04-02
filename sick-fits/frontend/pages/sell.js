e74cb4a848a125882a4acaa33ac72b167f721762
Edward Heath <e42291+github@gmail.com>
2019-04-01 19:01:48 -0700

    Complete 04

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 
 const Sell = props => (
-        <div>
-                <p>Hey!</p>
-                <Link href="/">
-                        <a>Home!</a>
-                </Link>
-        </div>
+  <div>
+    <p>Hey!</p>
+  </div>
 );
 
 export default Sell;
